20210432134268399ea9ebc2857547cf71d6d9a9
Dipika-Badadhe <157483969+Dipika-Badadhe@users.noreply.github.com>
2024-01-31 11:51:14 +0530

    Add files via upload

---
+++ Assignment Operator.java
@@ -0,0 +1,26 @@
+
+//Subtraction Operator
+
+import java.io.*;
+ 
+
+class Subtraction {
+
+    public static void main(String[] args)
+
+    {
+
+        int num1 = 60, num2 = 23, sub = 0;
+
+
+        System.out.println("num1 = " + num1);
+
+        System.out.println("num2 = " + num2);
+ 
+
+        sub = num1 - num2;
+
+        System.out.println("Subtraction = " + sub);
+
+    }
+}
